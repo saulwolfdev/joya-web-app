54fddd20636134fb6ce1776cd6b0db3099563fda
saul alejos garay <estrellasoscuras1984@gmail.com>
2021-05-26 22:49:02 -0300

    Modal Login+ Register

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -17,7 +17,43 @@ const NavBar = () => {
                             <a className="navbar-brand main-logo" href="#" rel="home" style={logoImage}>Joya</a>
                             </h1>
                         </div>
-                     <div className="col menu-col public-nav">
+                              {/*CON LOGIN INICIO*/}
+                        <div class="col menu-col user-nav">
+	                    	<button class="navbar-toggler user-toggle hamburger is-closed" type="button" data-toggle="offcanvas" data-target="#main-menu-user" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                				<span class="hamb-top"></span>
+	                            <span class="hamb-middle"></span>
+	                            <span class="hamb-bottom"></span>
+	                        </button>
+	                        <nav class="navbar navbar-inverse navbar-fixed-top" id="main-menu" role="navigation">
+	                            <div class="nav sidebar-nav col">
+						         	<ul class="menu user-menu">
+						         		<li class="username d-lg-none"><span class="a">
+		                        			<i class="far fa-user-circle"></i>
+		                        			Carolina	                        			
+		                        		</span></li>
+		                        		<li class="ayuda"><a href="#">Ayuda</a></li>
+		                        		<li><a href="#" class="destacado">Solicitar limpieza de vidriera</a></li>
+		                        		<li class="separator"></li>
+		                        		<li class="user-item"><a href="#">Tus pedidos</a></li>
+		                        		<li class="user-options dropdown closed">
+		                        			<span class="a user-item">
+		                        				<i class="far fa-user-circle"></i>
+		                        				Carolina
+		                        				<i class="far fa-chevron-down"></i>
+		                        			</span>
+		                        			<ul class="sub-menu">
+		                        				<li><a href="#">Tus pedidos</a></li>
+		                        				<li><a href="#">Configuración de cuenta</a></li>
+		                        				<li><a href="#">Salir</a></li>
+		                        			</ul>
+		                        		</li>
+		                        	</ul>
+		                        </div>
+		                    </nav>
+	                    </div>
+                          {/*CON LOGIN FIN*/}
+                            {/* SIN LOGIN INICIO*/}
+                        <div className="col menu-col public-nav">
                             <button className="navbar-toggler hamburger is-closed" 
                                     type="button" data-toggle="offcanvas" 
                                     data-target="#main-menu" 
@@ -39,6 +75,7 @@ const NavBar = () => {
                                 </div>
                             </nav>
                         </div> 
+                          {/* SIN LOGIN FIN*/}
                     </div>
                 </div>
                 <SignInModal show={show} close={closeModalHandler}/>
--- components/signin-modal/signin-modal.js
+++ components/signin-modal/signin-modal.js
@@ -4,7 +4,57 @@ const SignInModal = ({show, close}) => {
 
 
     return (
-                <div className="modal fade accountModal show"  style={{display:show? "block":"none"}} 
+<>
+
+
+{/* REGISTER INICIO*/}
+
+<div class="modal fade accountModal show" id="registroModal"  style={{display:show? "block":"none"}}  tabindex="-1" aria-labelledby="registroModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"  onClick={close}>X</button>
+      <div class="modal-body">
+        <h4>Estás a un paso de tus vidrieras limpias</h4>
+        <p>Completá tus datos para terminar de registrar tu pedido.</p>
+
+        <form class="form">
+        	<div class="row">
+        		<div class="col form-element">
+					<label for="nombre" class="form-label">Nombre</label>
+					<input type="text" class="form-control" name="nombre"/>
+				</div>
+				<div class="col form-element">
+					<label for="apellido" class="form-label">Apellido</label>
+					<input type="text" class="form-control" name="apellido"/>
+				</div>
+        	</div>
+        	<div class="form-element">
+				<label for="email" class="form-label">Email</label>
+				<input type="email" class="form-control" name="email"/>
+			</div>
+			<div class="form-element">
+				<label for="password" class="form-label">Contraseña</label>
+				<input type="password" class="form-control" name="password"/>
+			</div>
+			<div class="form-element">
+				<p><input type="checkbox" />Quiero recibir novedades</p>
+			</div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Registrarme</button>
+        <p>Ya tenés cuenta en Joya? <a href="#"  data-bs-target="#loginModal" data-bs-toggle="modal" data-bs-dismiss="modal">Ingresá</a> </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+{/* REGISTER FIN*/}
+
+
+{/* LOGIN INICIO*/}
+
+        <div className="modal fade accountModal show"  style={{display:show? "block":"none"}} 
                  id="loginModal" tabIndex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
                 <div className="modal-dialog modal-lg">
                     <div className="modal-content">
@@ -30,7 +80,11 @@ const SignInModal = ({show, close}) => {
                         </div>
                     </div>
                 </div>
-            </div>);
+            </div> 
+         
+{/* LOGIN FIN*/}
+</>
+            );
 }
  
 export default SignInModal;
