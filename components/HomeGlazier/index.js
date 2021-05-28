6e4e5a2d057ad5addfa9356d6c4d89e9d2e19841
M. Nicolas <nicolas.monzon@magnetarsrl.com>
2021-05-28 01:34:27 -0300

    Se edita componente Home para el vidrierista

--- components/HomeGlazier/index.js
+++ components/HomeGlazier/index.js
@@ -1,5 +1,19 @@
+import BeGlazier from './BeGlazier'
+import GlazierProfits from './GlazierProfits'
+import GlazierPostulate from './GlazierPostulate'
+import GlazierFaq from './GlazierFaq'
+import GlazierPostulateWithinForm from './GlazierPostulateWithinForm'
+
 const HomeGlazier = () => {
     return (
-        <></>
+        <div className="main-content">
+            <BeGlazier/>
+            <GlazierProfits/>
+            <GlazierPostulate/>
+            <GlazierFaq/>
+            <GlazierPostulateWithinForm/>
+        </div>
     );
 }
+
+export default HomeGlazier;
--- pages/glazier/index.js
+++ pages/glazier/index.js
@@ -1,23 +1,13 @@
 import Header from '../../components/Header'
-import BeGlazier from '../../components/HomeGlazier/BeGlazier'
+import HomeGlazier from '../../components/HomeGlazier'
 import Footer from '../../components/Footer'
-import GlazierProfits from '../../components/HomeGlazier/GlazierProfits'
-import GlazierPostulate from '../../components/HomeGlazier/GlazierPostulate'
-import GlazierFaq from '../../components/HomeGlazier/GlazierFaq'
-import GlazierPostulateWithinForm from '../../components/HomeGlazier/GlazierPostulateWithinForm'
 
 export default function Glazier() {
     return (
         <div id="wrapper">
 		    <Header glazier={true}/>
-			<div className="main-content">
-				<BeGlazier/>
-				<GlazierProfits/>
-				<GlazierPostulate/>
-				<GlazierFaq/>
-				<GlazierPostulateWithinForm/>
-			</div>
-		 <Footer glazier={true}/>
-	</div>
+			<HomeGlazier/>
+			<Footer glazier={true}/>
+		</div>
     )
   }
