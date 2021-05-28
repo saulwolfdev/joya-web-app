faaf2e96bdf5f4599601b1cdf00f1c0fd11ae4f2
saul alejos garay <estrellasoscuras1984@gmail.com>
2021-05-27 22:46:12 -0300

    Limpiando fuentes

--- assets/js/vidrierista.js
+++
@@ -1,9 +0,0 @@
-
-const mainNav = require('./main-nav');
-
-document.addEventListener('DOMContentLoaded', function() {
-    mainNav.initiateNav();
-});
-
-const devHelper = require('./dev-helpers'); //ELIMINAR EN PRODUCCION
-devHelper.init();
