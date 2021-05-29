16581aa17edd9bd58bb0f10f5b4777abfac892a6
M. Nicolas <nicolas.monzon@magnetarsrl.com>
2021-05-29 18:38:54 -0300

    Se edita formulario de inicio de sesion

--- components/SignInModal/index.js
+++ components/SignInModal/index.js
@@ -10,7 +10,6 @@ const SignInModal = ({show, close, showSignUp}) => {
             <div className="modal-dialog modal-lg">
                 <div className="modal-content">
                     <SignInModal.Header close={close}/>
-                    <SignInModal.Form/>
                     <SignInModal.Footer switchModal={switchModal}/>
                 </div>
             </div>
@@ -22,34 +21,65 @@ const Header = ({close}) => <button type="button" className="btn-close" data-bs-
 SignInModal.Header = Header;
 
 const Footer = ({switchModal}) => {
+
+    /* const registerUser = async event => {
+        event.preventDefault()
+    
+        const res = await fetch('/api/register', {
+            body: JSON.stringify({
+                email: event.target.email.value,
+                password: event.target.password.value
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        })
+        
+        const status = res.status;
+        if(status === 200) {
+            const result = await res.json()
+            ...
+        }
+        // result.user => 'Ada Lovelace'
+    } */
+
+    const registerUser = async event => {
+        console.log("Hola");
+        let email = event.target.email.value;
+        let password = event.target.password.value;
+        if(email === 'carolina@example.com' && password === '123321') {
+            console.log("Success");
+        }
+        else {
+            console.log("Failed");
+        }
+    }
+    
     return (
-        <div className="modal-footer">
-            <button type="button" className="btn btn-primary">Ingresar</button>
-            <p>No tenés cuenta en Joya aún? <a href="#" data-bs-target="#registroModal" data-bs-toggle="modal" data-bs-dismiss="modal" onClick={switchModal}>Registrate</a> </p>
-        </div>
+        <>
+            <div className="modal-body">
+                <h4>Estás a un paso de tus vidrieras limpias</h4>
+                <p>Accedé a tu cuenta para registrar tu pedido.</p>
+
+                <form className="form" onSubmit={registerUser}>
+                    <div className="form-element">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="email" className="form-control" name="email"/>
+                    </div>
+                    <div className="form-element">
+                        <label htmlFor="password" className="form-label">Contraseña</label>
+                        <input type="password" className="form-control" name="password"/>
+                    </div>
+                </form>
+            </div>
+            <div className="modal-footer">
+                <button type="submit" className="btn btn-primary">Ingresar</button>
+                <p>No tenés cuenta en Joya aún? <a href="#" data-bs-target="#registroModal" data-bs-toggle="modal" data-bs-dismiss="modal" onClick={switchModal}>Registrate</a> </p>
+            </div>
+        </>
     )
 }
 SignInModal.Footer = Footer;
-
-const Form = () => {
-    return (
-        <div className="modal-body">
-            <h4>Estás a un paso de tus vidrieras limpias</h4>
-            <p>Accedé a tu cuenta para registrar tu pedido.</p>
-
-            <form className="form">
-                <div className="form-element">
-                    <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" name="email"/>
-                </div>
-                <div className="form-element">
-                    <label htmlFor="password" className="form-label">Contraseña</label>
-                    <input type="password" className="form-control" name="password"/>
-                </div>
-            </form>
-        </div>
-    )
-}
-SignInModal.Form = Form;
 
 export default SignInModal;
