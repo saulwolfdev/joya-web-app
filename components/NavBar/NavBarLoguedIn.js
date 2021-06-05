fdd185d01b2d0b8ff3fe34b6567d8a15ab13b2d6
saul alejos garay <estrellasoscuras1984@gmail.com>
2021-06-05 19:23:47 -0300

    correccion de login

--- components/NavBar/NavBarLoguedIn.js
+++ components/NavBar/NavBarLoguedIn.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-const NavBarLoguedIn = () => {
+const NavBarLoguedIn = ({usuarioLogueado}) => {
     return (
         <div className="col menu-col user-nav">
             <button className="navbar-toggler user-toggle hamburger is-closed" type="button" data-toggle="offcanvas" data-target="#main-menu-user" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,19 +8,22 @@ const NavBarLoguedIn = () => {
                 <span className="hamb-middle"></span>
                 <span className="hamb-bottom"></span>
             </button>
-            <NavBarLoguedIn.Menu/>
+            <NavBarLoguedIn.Menu usuarioLogueado={usuarioLogueado}/>
         </div>
     )
 }
 
-const Menu = () => {
+const Menu = ({usuarioLogueado}) => {
+
+    
+
     return (
         <nav className="navbar navbar-inverse navbar-fixed-top" id="main-menu" role="navigation">
             <div className="nav sidebar-nav col">
                 <ul className="menu user-menu">
                     <li className="username d-lg-none"><span className="a">
                         <i className="far fa-user-circle"/>
-                        Carolina	                        			
+                        {usuarioLogueado? usuarioLogueado.displayName? usuarioLogueado.displayName : usuarioLogueado.email : ""}
                     </span></li>
                     <li className="ayuda"><a href="#">Ayuda</a></li>
                     <li><Link href='/apply'><a className="destacado">Solicitar limpieza de vidriera</a></Link></li>
@@ -29,7 +32,7 @@ const Menu = () => {
                     <li className="user-options dropdown closed">
                         <span className="a user-item">
                             <i className="far fa-user-circle"/>
-                            Carolina
+                            {usuarioLogueado? usuarioLogueado.displayName? usuarioLogueado.displayName : usuarioLogueado.email : ""}
                             <i className="far fa-chevron-down"/>
                         </span>
                         <ul className="sub-menu">
--- components/SignUpModal.js
+++ components/SignUpModal.js
@@ -1,17 +1,56 @@
+//React
 import React from 'react'
+import {useState } from 'react';
+//Firebase
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
+
 
 const SignUpModal = ({show, close, showSignIn}) => {
+
+    //Log
+    const[errorMsg,setErrorMsg] = useState("");
+
     const switchModal = () => {
         close(true);
         showSignIn(true);
     }
+
+
+    async function registerUser(e){
+        e.preventDefault(e);
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const nombre = e.target.nombre.value;
+        const apellido = e.target.apellido.value;
+
+        const auth = getAuth();
+        const user = await createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            //encapsular metodo para blanquear formularios
+            Array.from(document.querySelectorAll("input")).forEach(
+                input => (input.value = "")
+                );
+            
+            updateProfile(userCredential.user,{
+                displayName: apellido + ", "  + nombre
+                
+            });
+            close(true);  
+          })
+          .catch((error) => {
+            setErrorMsg(error.code);
+          });
+            
+
+    };
+
     return (
-        <div className="modal fade accountModal show" id="registroModal"  style={{display:show? "block":"none"}}  tabIndex="-1" aria-labelledby="registroModalLabel" aria-hidden="true">
+        <div className="modal fade accountModal show" id="registroModal"  style={{display:show? "block":"none"}}  tabIndex={-1} aria-labelledby="registroModalLabel" aria-hidden="true">
             <div className="modal-dialog modal-lg">
                 <div className="modal-content">
                     <SignUpModal.Header close={close}/>
-                    <SignUpModal.Form/>
-                    <SignUpModal.Footer switchModal={switchModal}/>
+                    <SignUpModal.Form registerUser={registerUser} errorMsg={errorMsg}   />
+                    <SignUpModal.Footer switchModal={switchModal}  />
                 </div>
             </div>
         </div>
@@ -24,39 +63,46 @@ SignUpModal.Header = Header;
 const Footer = ({switchModal}) => {
     return (
         <div className="modal-footer">
-            <button type="button" className="btn btn-primary">Registrarme</button>
             <p>Ya tenés cuenta en Joya? <a href="#"  data-bs-target="#loginModal" data-bs-toggle="modal" data-bs-dismiss="modal" onClick={switchModal}>Ingresá</a> </p>
         </div>
     )
 }
 SignUpModal.Footer = Footer;
 
-const Form = () => {
+const Form = ({registerUser, errorMsg}) => {    
+
+
     return (
         <div className="modal-body">
             <h4>Estás a un paso de tus vidrieras limpias</h4>
             <p>Completá tus datos para terminar de registrar tu pedido.</p>
-            <form className="form">
+            <form className="form" onSubmit={registerUser}>
                 <div className="row">
                     <div className="col form-element">
                         <label htmlFor="nombre" className="form-label">Nombre</label>
-                        <input type="text" className="form-control" name="nombre"/>
+                        <input type="text" className="form-control" name="nombre"  />
                     </div>
                     <div className="col form-element">
                         <label htmlFor="apellido" className="form-label">Apellido</label>
-                        <input type="text" className="form-control" name="apellido"/>
+                        <input type="text" className="form-control" name="apellido"  />
                     </div>
                 </div>
                 <div className="form-element">
                     <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" name="email"/>
+                    <input type="email" className="form-control" name="email" />
                 </div>
                 <div className="form-element">
                     <label htmlFor="password" className="form-label">Contraseña</label>
-                    <input type="password" className="form-control" name="password"/>
+                    <input type="password" className="form-control" name="password"  />
                 </div>
                 <div className="form-element">
                     <p><input type="checkbox" />Quiero recibir novedades</p>
+                </div>
+                <div className="error-msg">
+                    <i className="far fa-exclamation-circle"></i> {errorMsg}
+                </div>
+                <div className="modal-footer">
+                    <button type="submit" className="btn btn-primary">Registrarme</button>
                 </div>
             </form>
         </div>
