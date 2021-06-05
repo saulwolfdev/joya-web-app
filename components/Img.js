165654ae26de91e4a97a0debc39ff86380791be4
M. Nicolas <nicolas.monzon@magnetarsrl.com>
2021-06-05 18:31:16 -0300

    Tag img

--- components/Img.js
+++ components/Img.js
@@ -1,8 +1,6 @@
-import Image from 'next/image'
-
 const Img = (props) => {
     return (
-        <Image src={props.src} alt={props.alt} width={props.width} height={props.height}/> 
+        <img src={props.src} alt={props.alt} width={props.width} height={props.height}/> 
     );
 }
 
