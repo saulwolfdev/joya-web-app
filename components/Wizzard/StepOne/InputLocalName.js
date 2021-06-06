210501d3b6d4f2a0bbc32b8d1fb509416c4cd721
M. Nicolas <nicolas.monzon@magnetarsrl.com>
2021-06-06 00:57:58 -0300

    Fix

--- components/Wizzard/StepOne/InputLocalName.js
+++ components/Wizzard/StepOne/InputLocalName.js
@@ -1,10 +1,32 @@
-const InputLocalName = () => {
-    return (
-        <div className="form-element">
-            <label htmlFor="nombre_local" className="form-label">Nombre del local</label>
-            <input type="text" className="form-control" placeholder="Nombre del local..." name="nombre_local"/>
-        </div>
-    );
+import React, { Component } from "react";
+
+class InputLocalName extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            
+        };
+        this.onInputchange = this.onInputchange.bind(this);
+    }
+    
+    onInputchange(event) {
+        this.setState({
+            [event.target.value]: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div className="form-element">
+                <label htmlFor="nombre_local" className="form-label">Nombre del local</label>
+                <select className="form-control" placeholder="Nombre del local..." name="nombre_local" aria-label="Nombre del local...">
+                    <option defaultValue>Seleccioná un local</option>
+                    <option value="1">+Nuevo local</option>
+                </select>
+            </div>
+        );
+    }
 }
 
 export default InputLocalName;
