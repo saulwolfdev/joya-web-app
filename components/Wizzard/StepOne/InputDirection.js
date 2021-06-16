4e5326c2dfecd00d4e1d5020b49692c0dd76873c
M. Nicolas <nicolas.monzon@magnetarsrl.com>
2021-06-16 01:15:18 -0300

    Fix input

--- components/Wizzard/StepOne/InputDirection.js
+++ components/Wizzard/StepOne/InputDirection.js
@@ -1,17 +1,11 @@
-import React, { Component } from "react";
-
 const InputDirection = ({inputDirection, handleInputDirection}) => {
-    
-    const onInputchange = (event) => {
-        handleInputDirection(event.target.value);
-    }
 
     return (
         <div className="form-element">
             <label htmlFor="direccion" className="form-label">Dirección</label>
             <div className="input-group">
                 <span className="input-group-text"><i className="far fa-map-marker-alt"/></span>
-                <input type="text" className="form-control" placeholder="Dirección del local..." value={inputDirection} onChange={onInputchange} name="direction"/>
+                <input type="text" className="form-control" placeholder="Dirección del local..." defaultValue={inputDirection} onChange={handleInputDirection} name="direction"/>
             </div>
             <p className="form-text">El servicio sólo aplica a vidrieras en planta baja, de un máximo de 4 metros de altura.</p> 
         </div>
