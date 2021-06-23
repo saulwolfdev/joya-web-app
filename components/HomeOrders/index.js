357d656fb39a58fe04403af8920ff4aea4144833
M. Nicolas <nicolas.monzon@magnetarsrl.com>
2021-06-22 23:02:00 -0300

    Fix

--- components/HomeOrders/index.js
+++ components/HomeOrders/index.js
@@ -18,70 +18,9 @@ const HomeOrders = () => {
                         <div className="col-12 card-page-container">
                             <div className="card-page-content">
                                 <div className="inner-container">
-                                   <HomeOrders.Header/>
-                                    <div className="pedidos-pasados">
-                                        <div className="visita-pasada row">
-                                            <div className="col nombre-local">
-                                                Kentucky Santa Fe
-                                            </div>
-                                            <div className="col detalle">
-                                                <h5>Lunes 20 de Dic. <span>/ Entre 9 y 10hs</span></h5> 
-                                                <p className="descripcion">$150 / <span className="aclaracion">Fabián Sanchez de Castaños</span></p>
-                                            </div>
-                                            <div className="col-auto acciones">
-                                                <a href="#">Detalles</a>
-                                            </div>
-                                        </div>
-                                        <div className="visita-pasada row">
-                                            <div className="col nombre-local">
-                                                Kentucky Santa Fe
-                                            </div>
-                                            <div className="col detalle">
-                                                <h5>Lunes 20 de Dic. <span>/ Entre 9 y 10hs</span></h5> 
-                                                <p className="descripcion">$150 / <span className="aclaracion">Fabián Sanchez de Castaños</span></p>
-                                            </div>
-                                            <div className="col-auto acciones">
-                                                <a href="#">Detalles</a>
-                                            </div>
-                                        </div>
-                                        <div className="visita-pasada row">
-                                            <div className="col nombre-local">
-                                                Kentucky Santa Fe
-                                            </div>
-                                            <div className="col detalle">
-                                                <h5>Lunes 20 de Dic. <span>/ Entre 9 y 10hs</span></h5> 
-                                                <p className="descripcion">$150 / <span className="aclaracion">Fabián Sanchez de Castaños</span></p>
-                                            </div>
-                                            <div className="col-auto acciones">
-                                                <a href="#">Detalles</a>
-                                            </div>
-                                        </div>
-                                        <div className="visita-pasada row">
-                                            <div className="col nombre-local">
-                                                Kentucky Santa Fe
-                                            </div>
-                                            <div className="col detalle">
-                                                <h5>Lunes 20 de Dic. <span>/ Entre 9 y 10hs</span></h5> 
-                                                <p className="descripcion">$150 / <span className="aclaracion">Fabián Sanchez de Castaños</span></p>
-                                            </div>
-                                            <div className="col-auto acciones">
-                                                <a href="#">Detalles</a>
-                                            </div>
-                                        </div>
-                                        <div className="visita-pasada row">
-                                            <div className="col nombre-local">
-                                                Kentucky Santa Fe
-                                            </div>
-                                            <div className="col detalle">
-                                                <h5>Lunes 20 de Dic. <span>/ Entre 9 y 10hs</span></h5> 
-                                                <p className="descripcion">$150 / <span className="aclaracion">Fabián Sanchez de Castaños</span></p>
-                                            </div>
-                                            <div className="col-auto acciones">
-                                                <a href="#">Detalles</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <HomeOrders.Footer/>
+                                    <HomeOrders.Header past={past}/>
+                                    <Orders/>
+                                    <HomeOrders.Footer totalPages={mockTotalPages}/>
                                 </div>
                             </div>
                         </div>
