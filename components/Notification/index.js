520b289636b8a9ccfac3b30290b6853f5f0cb756
M. Nicolas <nicolas.monzon@magnetarsrl.com>
2021-07-18 14:50:06 -0300

    Componente de notificacion

---
+++ components/Notification/index.js
@@ -0,0 +1,20 @@
+const Notification = ({view, message, type}) => {
+    let clazz = "joyaToast ns-box ns-other ns-effect-loadingcircle ";
+    if(type === "success" || type === "warning" || type === "error") {
+        clazz += type;
+    }
+    return (
+        <>
+        {view ? 
+            <div className={clazz}>
+                <div className="ns-box-inner"><p>{message}</p></div>
+                <span className="ns-close"></span>
+            </div>
+            :
+            <></>
+        }
+        </>
+    )
+}
+
+export default Notification;
