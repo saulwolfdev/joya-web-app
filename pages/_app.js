a207a5a0d82d528439ed80460c8ffd8ce80024cc
saul alejos garay <estrellasoscuras1984@gmail.com>
2021-08-07 19:38:02 -0300

    Actualizacion de estilos,Admin

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../assets/styles/globals.css';
-import '../assets/fontawesome/css/all.css';
-
+import '../assets/fontawesome/css/all.css'; 
 import '../assets/styles/main.scss';
 
 import initializeFirebase from '../utils/firebaseConfig.ts';
