85b59111a69c429f85f1eb7464a131f4edda37c7
Nicolas Monzon <nicolas.monzon@wenance.com>
2021-09-15 16:47:56 -0300

    Pages 2

---
+++ components/HomeAllLocalsAdmin/data.js
@@ -0,0 +1,334 @@
+export const mockData = {
+    current: 2,
+    total: 3,
+    value: [
+        {
+            key: "001",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "toReview"
+        },
+        {
+            key: "002",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "ok"
+        },
+        {
+            key: "003",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "toReview"
+        },
+        {
+            key: "004",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "ok"
+        },
+        {
+            key: "005",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "toReview"
+        },
+        {
+            key: "006",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "ok"
+        },
+        {
+            key: "007",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "toReview"
+        },
+        {
+            key: "008",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "ok"
+        },
+        {
+            key: "009",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "toReview"
+        },
+        {
+            key: "010",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "ok"
+        },
+        {
+            key: "011",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "toReview"
+        },
+        {
+            key: "012",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "ok"
+        },
+        {
+            key: "013",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "toReview"
+        },
+        {
+            key: "014",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "ok"
+        },
+        {
+            key: "015",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "toReview"
+        },
+        {
+            key: "016",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "ok"
+        },
+        {
+            key: "017",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "toReview"
+        },
+        {
+            key: "018",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "ok"
+        },
+        {
+            key: "019",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "toReview"
+        },
+        {
+            key: "020",
+            state: "circle",
+            name: {
+                value: "Café Valerio",
+                info: "Desde 07/04/2020"
+            },
+            city: {
+                value: "Balvanera",
+                info: "Jujuy 232"
+            },
+            sup: "58",
+            client: "Grant Marshall",
+            lastOrder: "7 May",
+            state: "ok"
+        }
+    ]
+}
+
+export const mockMetrics = {
+    total: "105",
+    totalActive: "80",
+    totalNew: "75",
+    percentVsLastMonth: "-2",
+    pendings: "25"
+}
