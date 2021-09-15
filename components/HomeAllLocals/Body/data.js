b8e39530d5c6e4b9b327802b9a113fc26bbdc091
Nicolas Monzon <nicolas.monzon@wenance.com>
2021-09-15 17:19:28 -0300

    Reactor edit local 2

---
+++ components/HomeAllLocals/Body/data.js
@@ -0,0 +1,64 @@
+const mockData = {
+    current: 2,
+    total: 3,
+    value: [
+        {
+            key:"01",
+            name:"Kentucky Santa Fe",
+            direction:"Av. Santa Fe 2702, Palermo, Comuna 12",
+            surface:"64",
+            surfaceConfirmed: true,
+            surfaceBefore:"",
+            time:"10 minutos",
+            orders:"254",
+            pendingAvatar:false,
+            avatar:"url(/beneficios-side.jpg)",
+            verifiedAccount:true,
+            newLocal:false
+        },
+        {
+            key:"02",
+            name:"Kentucky Santa Fe",
+            direction:"Av. Santa Fe 2702, Palermo, Comuna 12",
+            surface:"64",
+            surfaceConfirmed: true,
+            surfaceBefore:"",
+            time:"10 minutos",
+            orders:"254",
+            pendingAvatar:true,
+            avatar:"",
+            verifiedAccount:false,
+            newLocal:false
+        },
+        {
+            key:"03",
+            name:"Kentucky Santa Fe",
+            direction:"Av. Santa Fe 2702, Palermo, Comuna 12",
+            surface:"80",
+            surfaceConfirmed: false,
+            surfaceBefore:"64",
+            time:"10 minutos",
+            orders:"254",
+            pendingAvatar:false,
+            avatar:"url(/beneficios-side.jpg)",
+            verifiedAccount:false,
+            newLocal:false
+        },
+        {
+            key:"04",
+            name:"Kentucky Santa Fe",
+            direction:"Av. Santa Fe 2702, Palermo, Comuna 12",
+            surface:"80",
+            surfaceConfirmed: false,
+            surfaceBefore:"64",
+            time:"10 minutos",
+            orders:"254",
+            pendingAvatar:true,
+            avatar:"",
+            verifiedAccount:false,
+            newLocal:true
+        }
+    ]
+}
+
+export default mockData;
---
+++ components/HomeAllLocals/EditLocal/data.js
@@ -0,0 +1,196 @@
+export const mockLocal = {
+    id: null,
+    name: "Kentucky Santa Fe",
+    direction: "Av. Santa Fe 2702",
+    zone: "Palermo, Comuna 12",
+    surface: "64",
+    time: "10 minutos",
+    additionalIndications: '',
+    record: "11/12/2021",
+    lastOrder: "Mi 15/4/2020, 15:05hs",
+    totalOrder: "254",
+    img: "kentucky.jpg"
+}
+
+export const listMockData = [
+    {
+        key: "001",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: true,
+        name: "Juan Perez"
+    },
+    {
+        key: "002",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "003",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "004",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: true,
+        name: "Juan Perez"
+    },
+    {
+        key: "005",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "006",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "007",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: true,
+        name: "Juan Perez"
+    },
+    {
+        key: "008",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "009",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "010",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: true,
+        name: "Juan Perez"
+    },
+    {
+        key: "011",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "012",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "013",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: true,
+        name: "Juan Perez"
+    },
+    {
+        key: "014",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "015",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "016",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: true,
+        name: "Juan Perez"
+    },
+    {
+        key: "017",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "018",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    },
+    {
+        key: "019",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: true,
+        name: "Juan Perez"
+    },
+    {
+        key: "020",
+        date: "Mie 07/04/2020",
+        timeRange: "11:00 - 17:00",
+        price: "$7186",
+        state: "Completado", // TODO: enum
+        report: false,
+        name: "Juan Perez"
+    }
+]
